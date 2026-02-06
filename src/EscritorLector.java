a68883694a1226b37a4e788196cb418a3b4c7349
Dibo72 <humanida133@gmail.com>
2026-02-06 09:31:36 +0100

    mejora

--- src/EscritorLector.java
+++ src/EscritorLector.java
@@ -7,16 +7,8 @@ import java.util.ArrayList;
 public class EscritorLector {
     public static void main(String[] args) {
         ArrayList<String> lista = new ArrayList<String>();
-        lista.add("El sol salió temprano y llenó la mañana de luz." + "\n" +
-
-                "A veces el silencio dice más que mil palabras." + "\n" +
-
-                "Aprender algo nuevo cambia la forma de ver el mundo." + "\n" +
-
-                "Un café caliente puede arreglar un día complicado." + "\n" +
-
-                "Los pequeños detalles suelen marcar la diferencia.");
-        try(BufferedWriter escritor = new BufferedWriter(new FileWriter("archivo.txt"))) {
+        lista.add("Juan" + "\n" + "Pepe" + "\n" + "Felpe" + "\n" + "Carlos" + "\n" + "Ramon");
+        try(BufferedWriter escritor = new BufferedWriter(new FileWriter("nombres.txt"))) {
             for (String linea : lista) {
                 escritor.write(linea);
                 escritor.newLine();
@@ -25,7 +17,7 @@ public class EscritorLector {
             System.out.println(e);
         }
 
-        try(BufferedReader lector = new BufferedReader(new FileReader("archivo.txt"))){
+        try(BufferedReader lector = new BufferedReader(new FileReader("nombres.txt"))){
             String line;
             while ((line = lector.readLine()) != null){
                 System.out.println(line);
